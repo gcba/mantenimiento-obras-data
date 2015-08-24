6ba48b665b13ef5223af0b6545b82f8f52d96078
Carolina Hadad <caro@Carolinas-MacBook-Air.local>
2015-08-24 13:21:00 -0300

    Subidos nuevos WSDL de prod

--- api-avisos.py
+++ api-avisos.py
@@ -1,18 +1,24 @@
 from suds.client import Client
 import csv
-import urlparse, urllib, os
+import urlparse
+import urllib
+import os
 
-url = urlparse.urljoin('file:', urllib.pathname2url(os.path.abspath('I016 - Avisos QA.WSDL')))
-client = Client(url, username='GABIERTO', password="gab2015!!")
+url = urlparse.urljoin(
+    'file:', urllib.pathname2url(os.path.abspath('I016 - Avisos PRD.WSDL')))
 
+client = Client(url, username='GABIERTO', password="2x5=diez")  # PROD
 
-desde = "20150608"
-hasta = "20150614"
+# client = Client(url, username='GABIERTO', password="gab2015!!") QA
+
+desde = "20150501"
+hasta = "20150807"
 modo = "CREACION"
 
-columnas = ["NRO_AVISO","CLASE_AVISO","TEXTO_EXPLICATIVO","STATUS_USUARIO","GRP_PLANIFICADOR","PRO_RESPONSABLE","PRIORIDAD","FECHA_CREACION","FECHA_AVISO","HORA_AVISO","FECHA_INI_DESEADO","HORA_INI_DESEADO","FECHA_FIN_DESEADO","HORA_FIN_DESEADO","UBIC_TECNICA","CALLE","ALTURA","AREA_EMPRESA","EMP_OBJ_MANTENIM","LOCAL","CAMPO_CLASIF","FECHA_ULT_MODIF","GRP_PRESTACIONES","COD_PRESTACIONES","TXT_CODIGO","RESP_ULT_MODIF","NRO_ORDEN"]
+columnas = ["NRO_AVISO", "CLASE_AVISO", "TEXTO_EXPLICATIVO", "STATUS_USUARIO", "GRP_PLANIFICADOR", "PRO_RESPONSABLE", "PRIORIDAD", "FECHA_CREACION", "FECHA_AVISO", "HORA_AVISO", "FECHA_INI_DESEADO", "HORA_INI_DESEADO", "FECHA_FIN_DESEADO",
+            "HORA_FIN_DESEADO", "UBIC_TECNICA", "CALLE", "ALTURA", "AREA_EMPRESA", "EMP_OBJ_MANTENIM", "LOCAL", "CAMPO_CLASIF", "FECHA_ULT_MODIF", "GRP_PRESTACIONES", "COD_PRESTACIONES", "TXT_CODIGO", "RESP_ULT_MODIF", "NRO_ORDEN", "CODIFICACIONES"]
 #tipos_aviso = ["AP", "EM", "SU"]
-tipos_aviso = ["DI","OF","OP","RE","SU"]
+tipos_aviso = ["DI", "OF", "OP", "RE", "SU"]
 
 nombre_archivo = "avisos-" + desde + "-" + hasta + ".csv"
 
@@ -23,13 +29,13 @@ csv_writer = csv.writer(file_avisos, delimiter=';')
 csv_writer.writerow(columnas)
 
 for tipo_aviso in tipos_aviso:
-	result = client.service.si_gob_abierto(tipo_aviso, desde, hasta, modo)
-	for record in result:
-		record_values = []
-		for columna in columnas:
-			if record[columna] is not None:
-				record_value = record[columna].encode('utf8','ignore')
-			else:
-				record_value = ""
-			record_values.append(record_value)
-		csv_writer.writerow(record_values)
+    result = client.service.si_gob_abierto(tipo_aviso, desde, hasta, modo)
+    for record in result:
+        record_values = []
+        for columna in columnas:
+            if record[columna] is not None:
+                record_value = record[columna].encode('utf8', 'ignore')
+            else:
+                record_value = ""
+            record_values.append(record_value)
+        csv_writer.writerow(record_values)
--- api-ordenes.py
+++ api-ordenes.py
@@ -1,19 +1,24 @@
 from suds.client import Client
 import csv
 
-import urlparse, urllib, os
+import urlparse
+import urllib
+import os
 
-url = urlparse.urljoin('file:', urllib.pathname2url(os.path.abspath('I016 - Ordenes QA.WSDL')))
-client = Client(url, username='GABIERTO', password="gab2015!!")
+url = urlparse.urljoin(
+    'file:', urllib.pathname2url(os.path.abspath('I016 - Ordenes PRD.WSDL')))
+client = Client(url, username='GABIERTO', password="2x5=diez")  # PROD
+# QA: client = Client(url, username='GABIERTO', password="gab2015!!")
 
 
-desde = "20140801"
+desde = "20150501"
 hasta = "20150807"
 tipos_ordenes = ["ACRE", "CARE"]
 modo = "CREACION"
-columnas = ["NRO_ORDEN", "CLASE_ORDEN", "ORDEN_SUPERIOR", "ORDEN_INFERIOR", "DESCRIPCION", "UBIC_TECNICA_DESC", "STATUS_USUARIO", "GRP_PLANIFICADOR", "PTO_RESPONSABLE", "PRIORIDAD", "FECHA_CREACION", "FECHA_INI_EXTREMO", "FECHA_FIN_EXTREMO", "UBIC_TECNICA", "CALLE", "ALTURA", "AREA_EMPRESA", "EMP_OBJ_MANTENIM", "LOCAL", "CAMPO_CLASIF", "FECHA_ULT_MODIF", "RESP_ULT_MODIF", "CLAVE_MODELO", "CLAVE_MODELO_TXT", "DURACION", "STATUS_OPERACION"]
+columnas = ["NRO_ORDEN", "CLASE_ORDEN", "ORDEN_SUPERIOR", "ORDEN_INFERIOR", "DESCRIPCION", "UBIC_TECNICA_DESC", "STATUS_USUARIO", "GRP_PLANIFICADOR", "PTO_RESPONSABLE", "PRIORIDAD", "FECHA_CREACION", "FECHA_INI_EXTREMO",
+            "FECHA_FIN_EXTREMO", "UBIC_TECNICA", "CALLE", "ALTURA", "AREA_EMPRESA", "EMP_OBJ_MANTENIM", "LOCAL", "CAMPO_CLASIF", "FECHA_ULT_MODIF", "RESP_ULT_MODIF", "CLAVE_MODELO", "CLAVE_MODELO_TXT", "DURACION", "STATUS_OPERACION"]
 
-nombre_archivo = "ordenes-" + tipos_ordenes[0]+"-"+desde + "-" + hasta + ".csv" 
+nombre_archivo = "ordenes-" + desde + "-" + hasta + ".csv"
 
 file_ordenes = open(nombre_archivo, 'w')
 file_ordenes.seek(0)
@@ -22,14 +27,14 @@ csv_writer = csv.writer(file_ordenes, delimiter=';')
 csv_writer.writerow(columnas)
 
 for tipo_orden in tipos_ordenes:
-	result = client.service.si_gobabierto(tipo_orden, desde, hasta, modo)
-	print result
-	for record in result:
-		record_values = []
-		for columna in columnas:
-			if record[columna] is not None:
-				record_value = record[columna].encode('utf8','ignore')
-			else:
-				record_value = ""
-			record_values.append(record_value)
-		csv_writer.writerow(record_values)
+    result = client.service.si_gobabierto(tipo_orden, desde, hasta, modo)
+    print result
+    for record in result:
+        record_values = []
+        for columna in columnas:
+            if record[columna] is not None:
+                record_value = record[columna].encode('utf8', 'ignore')
+            else:
+                record_value = ""
+            record_values.append(record_value)
+        csv_writer.writerow(record_values)
