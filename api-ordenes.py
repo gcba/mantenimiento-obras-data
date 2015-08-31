6913370e1f7cd27ec64e79f97aad74090b69a9ae
Pili Mayora <pili.mayora@gmail.com>
2015-08-31 17:23:46 -0300

    Escribo a DB

--- api-ordenes.py
+++ api-ordenes.py
@@ -1,22 +1,58 @@
 from suds.client import Client
 import csv
 
+import config
 import urlparse
 import urllib
 import os
 
+from sqlalchemy import *
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+conn_str = "mysql://" + config.db["user"] + ":" + \
+    config.db["password"] + "@" + config.db["host"] + "/" + config.db["db"]
+engine = create_engine('mysql://root:5Gonz4lol@localhost/mantenimiento')
+# create a configured "Session" class
+Session = sessionmaker(bind=engine)
+# create a Session
+session = Session()
+Base = declarative_base()
+
+
+class Orden(Base):
+    __tablename__ = 'ordenes'
+    nro_orden = Column(String(50), primary_key=True)
+    clase_orden = Column(String(100))
+    descripcion = Column(String(100))
+    ubic_tecnica = Column(String(100))
+    ubic_tecnica_desc = Column(String(100))
+    fecha_creacion = Column(Date)
+    fecha_ini_extremo = Column(Date)
+    fecha_fin_extremo = Column(Date)
+    calle = Column(String(50))
+    altura = Column(String(10))
+    clave_modelo_txt = Column(String(50))
+    status_operacion = Column(String(50))
+
+    def __repr__(self):
+        return "<Orden(nro_orden='%s', clase_orden='%s', descripcion='%s', ubic_tecnica='%s', ubic_tecnica_desc='%s', fecha_creacion='%s', fecha_ini_extremo='%s', fecha_fin_extremo='%s', calle='%s', altura='%s', clave_modelo_txt='%s', status_operacion='%s')>" % (self.nro_orden, self.clase_orden, self.descripcion, self.ubic_tecnica, self.ubic_tecnica_desc, self.fecha_creacion,
+                                                                                                                                                                                                                                                                       self.fecha_ini_extremo, self.fecha_fin_extremo, self.calle, self.altura, self.clave_modelo_txt, self.status_operacion)
+
+Base.metadata.create_all(engine, checkfirst=True)
+
 url = urlparse.urljoin(
     'file:', urllib.pathname2url(os.path.abspath('I016 - Ordenes PRD.WSDL')))
 client = Client(url, username='GABIERTO', password="2x5=diez")  # PROD
-# QA: client = Client(url, username='GABIERTO', password="gab2015!!")
-
+# QA:
+#     client = Client(url, username='GABIERTO', password="gab2015!!")
 
 desde = "20150501"
 hasta = "20150807"
 tipos_ordenes = ["ACRE", "CARE"]
 modo = "CREACION"
-columnas = ["NRO_ORDEN", "CLASE_ORDEN", "ORDEN_SUPERIOR", "ORDEN_INFERIOR", "DESCRIPCION", "UBIC_TECNICA_DESC", "STATUS_USUARIO", "GRP_PLANIFICADOR", "PTO_RESPONSABLE", "PRIORIDAD", "FECHA_CREACION", "FECHA_INI_EXTREMO",
-            "FECHA_FIN_EXTREMO", "UBIC_TECNICA", "CALLE", "ALTURA", "AREA_EMPRESA", "EMP_OBJ_MANTENIM", "LOCAL", "CAMPO_CLASIF", "FECHA_ULT_MODIF", "RESP_ULT_MODIF", "CLAVE_MODELO", "CLAVE_MODELO_TXT", "DURACION", "STATUS_OPERACION"]
+columnas = ["NRO_ORDEN", "CLASE_ORDEN", "DESCRIPCION", "UBIC_TECNICA", "UBIC_TECNICA_DESC", "FECHA_CREACION", "FECHA_INI_EXTREMO",
+            "FECHA_FIN_EXTREMO",  "CALLE", "ALTURA", "CLAVE_MODELO_TXT", "STATUS_OPERACION"]
 
 nombre_archivo = "ordenes-" + desde + "-" + hasta + ".csv"
 
@@ -30,11 +66,19 @@ for tipo_orden in tipos_ordenes:
     result = client.service.si_gobabierto(tipo_orden, desde, hasta, modo)
     print result
     for record in result:
+        new_orden = {}
         record_values = []
-        for columna in columnas:
-            if record[columna] is not None:
-                record_value = record[columna].encode('utf8', 'ignore')
-            else:
-                record_value = ""
-            record_values.append(record_value)
-        csv_writer.writerow(record_values)
+    for columna in columnas:
+        if record[columna] is not None:
+            new_orden[columna.lower()] = record[columna].encode(
+                'utf8', 'ignore')
+            record_value = record[columna].encode('utf8', 'ignore')
+        else:
+            new_orden[columna.lower()] = ""
+            record_value = ""
+        record_values.append(record_value)
+    # Add record to DB
+    session.add(Orden(**new_orden))
+    session.commit()
+    # Write record to CSV
+    csv_writer.writerow(record_values)
