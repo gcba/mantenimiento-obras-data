ba57c95c2935befc3aaae37af311b705540a69ec
Pili Mayora <pili.mayora@gmail.com>
2015-09-29 17:49:01 -0300

    Arreglo el sample de config. Geolocalizo las direcciones de obras antes de guardarlas en la DB

--- api-ordenes.py
+++ api-ordenes.py
@@ -1,15 +1,15 @@
 from suds.client import Client
-import csv
 
 import config
 import urlparse
 import urllib
 import os
+import json
+import urllib2
 
 from sqlalchemy import *
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-
 
 engine = create_engine(config.db_url)
 # create a configured "Session" class
@@ -27,6 +27,9 @@ class Orden(Base):
     descripcion = Column(String(100))
     ubic_tecnica = Column(String(100))
     ubic_tecnica_desc = Column(String(100))
+    geo_x = Column(String(20))
+    geo_y = Column(String(20))
+    tipo_resultado = Column(String(100))
     fecha_creacion = Column(Date)
     fecha_ini_extremo = Column(Date)
     fecha_fin_extremo = Column(Date)
@@ -50,16 +53,12 @@ client = Client(
     url, username=config.wsdl_username, password=config.wsdl_password)
 
 desde = "20150801"
-hasta = "20150807"
+hasta = "20150803"
 tipos_ordenes = ["ACRE", "CARE"]
 modo = "CREACION"
 columnas = ["NRO_ORDEN", "CLASE_ORDEN", "DESCRIPCION", "UBIC_TECNICA", "UBIC_TECNICA_DESC", "FECHA_CREACION", "FECHA_INI_EXTREMO",
             "FECHA_FIN_EXTREMO",  "CALLE", "ALTURA", "CLAVE_MODELO", "CLAVE_MODELO_TXT", "AREA_EMPRESA", "STATUS_USUARIO", "FECHA_ULT_MODIF"]
 
-nombre_archivo = "ordenes-" + desde + "-" + hasta + ".csv"
-
-file_ordenes = open(nombre_archivo, 'w')
-file_ordenes.seek(0)
 
 for tipo_orden in tipos_ordenes:
     result = client.service.si_gobabierto(tipo_orden, desde, hasta, modo)
@@ -72,6 +71,22 @@ for tipo_orden in tipos_ordenes:
                     'utf8', 'ignore')
             else:
                 new_orden[columna.lower()] = None
+        direccion_list = new_orden[
+            "ubic_tecnica_desc"].split("-")[0].split(" ")
+        altura = direccion_list[-1]
+        calle = " ".join(direccion_list[:-1]).replace(" ", "%20")
+
+        geocod = json.load(urllib2.urlopen(
+            'http://ws.usig.buenosaires.gob.ar/rest/normalizar_y_geocodificar_direcciones?calle=' + calle + '&altura=' + altura + '&desambiguar=1'))
+        if geocod["Normalizacion"]:
+            new_orden["geo_x"] = ""
+            new_orden["geo_y"] = ""
+            new_orden["tipo_resultado"] = geocod[
+                "Normalizacion"]["TipoResultado"]
+        if geocod["GeoCodificacion"]:
+            new_orden["geo_x"] = geocod["GeoCodificacion"]["x"]
+            new_orden["geo_y"] = geocod["GeoCodificacion"]["y"]
+
         ev = session.query(Orden).filter(Orden.nro_orden == new_orden[
             'nro_orden'], Orden.clave_modelo == new_orden['clave_modelo']).count()
         if not ev:
