b13b16e90ae4e0fc6534ae36a32e8919601ad67e
Pili Mayora <pili.mayora@gmail.com>
2015-10-15 16:33:54 -0300

    Pido obras de todos los tipos y empiezo a probar agregarle comunas

--- api-ordenes.py
+++ api-ordenes.py
@@ -40,10 +40,11 @@ class Orden(Base):
     area_empresa = Column(String(10))
     status_usuario = Column(String(50))
     fecha_ult_modif = Column(Date)
+    comuna = Column(String(2))
 
     def __repr__(self):
-        return "<Orden(nro_orden='%s', clase_orden='%s', descripcion='%s', ubic_tecnica='%s', ubic_tecnica_desc='%s', fecha_creacion='%s', fecha_ini_extremo='%s', fecha_fin_extremo='%s', calle='%s', altura='%s', clave_modelo_txt='%s', status_operacion='%s')>" % (self.nro_orden, self.clase_orden, self.descripcion, self.ubic_tecnica, self.ubic_tecnica_desc, self.fecha_creacion,
-                                                                                                                                                                                                                                                                       self.fecha_ini_extremo, self.fecha_fin_extremo, self.calle, self.altura, self.clave_modelo_txt, self.status_operacion)
+        return "<Orden(nro_orden='%s', clase_orden='%s', descripcion='%s', ubic_tecnica='%s', ubic_tecnica_desc='%s', geo_x='%s', geo_y='%s', fecha_creacion='%s', fecha_ini_extremo='%s', fecha_fin_extremo='%s', calle='%s', altura='%s', clave_modelo_txt='%s', status_operacion='%s', comuna='%s')>" % (self.nro_orden, self.clase_orden, self.descripcion, self.ubic_tecnica, self.ubic_tecnica_desc, self.geo_x, self.geo_y, self.fecha_creacion,
+                                                                                                                                                                                                                                                                                                            self.fecha_ini_extremo, self.fecha_fin_extremo, self.calle, self.altura, self.clave_modelo_txt, self.status_operacion, self.comuna)
 
 Base.metadata.create_all(engine, checkfirst=True)
 
@@ -52,9 +53,9 @@ url = urlparse.urljoin(
 client = Client(
     url, username=config.wsdl_username, password=config.wsdl_password)
 
-desde = "20150822"
+desde = "20150823"
 hasta = "20150831"
-tipos_ordenes = ["ACRE", "CARE"]
+tipos_ordenes = ["ACRE", "ACME", "CARE", "CAME"]
 modo = "CREACION"
 columnas = ["NRO_ORDEN", "CLASE_ORDEN", "DESCRIPCION", "UBIC_TECNICA", "UBIC_TECNICA_DESC", "FECHA_CREACION", "FECHA_INI_EXTREMO",
             "FECHA_FIN_EXTREMO",  "CALLE", "ALTURA", "CLAVE_MODELO", "CLAVE_MODELO_TXT", "AREA_EMPRESA", "STATUS_USUARIO", "FECHA_ULT_MODIF"]
@@ -74,7 +75,8 @@ for tipo_orden in tipos_ordenes:
         direccion_list = new_orden[
             "ubic_tecnica_desc"].split("-")[0].split(" ")
         altura = direccion_list[-1]
-        calle = " ".join(direccion_list[:-1]).replace(" ", "%20")
+        calle = " ".join(
+            direccion_list[:-1]).replace(",", "").replace(" ", "%20")
 
         geocod = json.load(urllib2.urlopen(
             'http://ws.usig.buenosaires.gob.ar/rest/normalizar_y_geocodificar_direcciones?calle=' + calle + '&altura=' + altura + '&desambiguar=1'))
@@ -86,6 +88,16 @@ for tipo_orden in tipos_ordenes:
         if "GeoCodificacion" in geocod.keys():
             new_orden["geo_x"] = geocod["GeoCodificacion"]["x"]
             new_orden["geo_y"] = geocod["GeoCodificacion"]["y"]
+
+        # comuna = "na"
+        # try:
+        #     res_comuna = json.load(urllib2.urlopen(
+        #         'http://ws.usig.buenosaires.gob.ar/datos_utiles?calle=' + calle + '&altura=' + altura))
+        # except ValueError:
+        #     print "Calle y altura inexistentes"
+        # else:
+        #     print "Otro tipo de error ocurrio en la request"
+        # print comuna
 
         ev = session.query(Orden).filter(Orden.nro_orden == new_orden[
             'nro_orden'], Orden.clave_modelo == new_orden['clave_modelo']).count()
