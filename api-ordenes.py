17d84fb9941ff343ffc121805f2a7663a995d62d
Pili Mayora <pili.mayora@gmail.com>
2015-10-30 17:35:54 -0300

    Pido obras de medicion nada mas, pero en modo creacion Y modificacion. Solo geolocalizo las direcciones y tomo su comuna si la obra no la tenemos ya en la base. A evitar repetidos

--- api-ordenes.py
+++ api-ordenes.py
@@ -40,7 +40,7 @@ class Orden(Base):
     area_empresa = Column(String(10))
     status_usuario = Column(String(50))
     fecha_ult_modif = Column(Date)
-    comuna = Column(String(2))
+    comuna = Column(String(20))
 
     def __repr__(self):
         return "<Orden(nro_orden='%s', clase_orden='%s', descripcion='%s', ubic_tecnica='%s', ubic_tecnica_desc='%s', geo_x='%s', geo_y='%s', fecha_creacion='%s', fecha_ini_extremo='%s', fecha_fin_extremo='%s', calle='%s', altura='%s', clave_modelo_txt='%s', status_operacion='%s', comuna='%s')>" % (self.nro_orden, self.clase_orden, self.descripcion, self.ubic_tecnica, self.ubic_tecnica_desc, self.geo_x, self.geo_y, self.fecha_creacion,
@@ -53,55 +53,60 @@ url = urlparse.urljoin(
 client = Client(
     url, username=config.wsdl_username, password=config.wsdl_password)
 
-desde = "20150823"
-hasta = "20150831"
-tipos_ordenes = ["ACRE", "ACME", "CARE", "CAME"]
-modo = "CREACION"
+desde = "20150901"
+hasta = "20150902"
+tipos_ordenes = ["ACME", "CAME"]
+modos = ["CREACION", "MODIFICACION"]
 columnas = ["NRO_ORDEN", "CLASE_ORDEN", "DESCRIPCION", "UBIC_TECNICA", "UBIC_TECNICA_DESC", "FECHA_CREACION", "FECHA_INI_EXTREMO",
             "FECHA_FIN_EXTREMO",  "CALLE", "ALTURA", "CLAVE_MODELO", "CLAVE_MODELO_TXT", "AREA_EMPRESA", "STATUS_USUARIO", "FECHA_ULT_MODIF"]
 
-
 for tipo_orden in tipos_ordenes:
-    result = client.service.si_gobabierto(tipo_orden, desde, hasta, modo)
-    print result
-    for record in result:
-        new_orden = {}
-        for columna in columnas:
-            if record[columna] is not None:
-                new_orden[columna.lower()] = record[columna].encode(
-                    'utf8', 'ignore')
-            else:
-                new_orden[columna.lower()] = None
-        direccion_list = new_orden[
-            "ubic_tecnica_desc"].split("-")[0].split(" ")
-        altura = direccion_list[-1]
-        calle = " ".join(
-            direccion_list[:-1]).replace(",", "").replace(" ", "%20")
+    for modo in modos:
+        result = client.service.si_gobabierto(tipo_orden, desde, hasta, modo)
+        print "llamando a la API con valores " + tipo_orden + " y " + modo + ""
+        print "la API returneo con " + str(len(result)) + " resultados"
+        for record in result:
+            new_orden = {}
+            for columna in columnas:
+                if record[columna] is not None:
+                    new_orden[columna.lower()] = record[columna].encode(
+                        'utf8', 'ignore')
+                else:
+                    new_orden[columna.lower()] = None
 
-        geocod = json.load(urllib2.urlopen(
-            'http://ws.usig.buenosaires.gob.ar/rest/normalizar_y_geocodificar_direcciones?calle=' + calle + '&altura=' + altura + '&desambiguar=1'))
-        if "Normalizacion" in geocod.keys():
-            new_orden["geo_x"] = ""
-            new_orden["geo_y"] = ""
-            new_orden["tipo_resultado"] = geocod[
-                "Normalizacion"]["TipoResultado"]
-        if "GeoCodificacion" in geocod.keys():
-            new_orden["geo_x"] = geocod["GeoCodificacion"]["x"]
-            new_orden["geo_y"] = geocod["GeoCodificacion"]["y"]
+            ev = session.query(Orden).filter(Orden.nro_orden == new_orden[
+                'nro_orden'], Orden.clave_modelo == new_orden['clave_modelo']).count()
+            if not ev:
+                # Si no existe el record en la DB, calcular geocodificacion,
+                # comuna, y agregar a la DB
 
-        # comuna = "na"
-        # try:
-        #     res_comuna = json.load(urllib2.urlopen(
-        #         'http://ws.usig.buenosaires.gob.ar/datos_utiles?calle=' + calle + '&altura=' + altura))
-        # except ValueError:
-        #     print "Calle y altura inexistentes"
-        # else:
-        #     print "Otro tipo de error ocurrio en la request"
-        # print comuna
+                direccion_list = new_orden[
+                    "ubic_tecnica_desc"].split("-")[0].split(" ")
+                altura = direccion_list[-1]
+                calle = " ".join(
+                    direccion_list[:-1]).replace(",", "").replace("PARCELA", "").replace(" ", "%20")
 
-        ev = session.query(Orden).filter(Orden.nro_orden == new_orden[
-            'nro_orden'], Orden.clave_modelo == new_orden['clave_modelo']).count()
-        if not ev:
-            # Add record to DB
-            session.add(Orden(**new_orden))
-            session.commit()
+                geocod = json.load(urllib2.urlopen(
+                    'http://ws.usig.buenosaires.gob.ar/rest/normalizar_y_geocodificar_direcciones?calle=' + calle + '&altura=' + altura + '&desambiguar=1'))
+                if "Normalizacion" in geocod.keys():
+                    new_orden["geo_x"] = ""
+                    new_orden["geo_y"] = ""
+                    new_orden["tipo_resultado"] = geocod[
+                        "Normalizacion"]["TipoResultado"]
+                if "GeoCodificacion" in geocod.keys():
+                    new_orden["geo_x"] = geocod["GeoCodificacion"]["x"]
+                    new_orden["geo_y"] = geocod["GeoCodificacion"]["y"]
+
+                comuna = "NA"
+                try:
+                    res_comuna = json.load(urllib2.urlopen(
+                        'http://ws.usig.buenosaires.gob.ar/datos_utiles?calle=' + calle + '&altura=' + altura))
+                    comuna = res_comuna["comuna"].split(" ")[1]
+                except ValueError as e:
+                    comuna = "Error direccion"
+                except:
+                    comuna = "Error OTRO"
+                new_orden["comuna"] = comuna
+
+                session.add(Orden(**new_orden))
+                session.commit()
