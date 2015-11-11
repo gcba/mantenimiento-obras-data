38f4878623a2ed2ffa6f2b3006d8a434d46c98dd
Pili Mayora <pili.mayora@gmail.com>
2015-11-11 18:39:18 -0300

    Agrego parseo de status a un estado de obra humano

--- api-ordenes.py
+++ api-ordenes.py
@@ -6,6 +6,11 @@ import urllib
 import os
 import json
 import urllib2
+import csv
+
+###########################################################
+# HUGE TODO: refactor sqlalchemy stuff to separate module #
+###########################################################
 
 from sqlalchemy import *
 from sqlalchemy.orm import sessionmaker
@@ -17,6 +22,24 @@ Session = sessionmaker(bind=engine)
 # create a Session
 session = Session()
 Base = declarative_base()
+
+
+class TipoObra(Base):
+    __tablename__ = 'tipos_obra'
+    tipo_id = Column(Integer, primary_key=True, autoincrement='ignore_fk')
+    name = Column(String(50))
+
+    def __repr__(self):
+        return "<TipoObra(name='%s')>" % (self.name)
+
+
+class StatusObra(Base):
+    __tablename__ = 'status_obra'
+    status_id = Column(Integer, primary_key=True, autoincrement='ignore_fk')
+    name = Column(String(50))
+
+    def __repr__(self):
+        return "<StatusObra(name='%s')>" % (self.name)
 
 
 class Orden(Base):
@@ -37,24 +60,83 @@ class Orden(Base):
     altura = Column(String(10))
     clave_modelo = Column(String(50))
     clave_modelo_txt = Column(String(100))
+    tipo_obra_id = Column(Integer, ForeignKey('tipos_obra.tipo_id'))
     area_empresa = Column(String(10))
     status_usuario = Column(String(50))
+    status_id = Column(Integer, ForeignKey('status_traduccion.status_id'))
     fecha_ult_modif = Column(Date)
     comuna = Column(String(20))
 
     def __repr__(self):
-        return "<Orden(nro_orden='%s', clase_orden='%s', descripcion='%s', ubic_tecnica='%s', ubic_tecnica_desc='%s', geo_x='%s', geo_y='%s', fecha_creacion='%s', fecha_ini_extremo='%s', fecha_fin_extremo='%s', calle='%s', altura='%s', clave_modelo_txt='%s', status_operacion='%s', comuna='%s')>" % (self.nro_orden, self.clase_orden, self.descripcion, self.ubic_tecnica, self.ubic_tecnica_desc, self.geo_x, self.geo_y, self.fecha_creacion,
-                                                                                                                                                                                                                                                                                                            self.fecha_ini_extremo, self.fecha_fin_extremo, self.calle, self.altura, self.clave_modelo_txt, self.status_operacion, self.comuna)
+        return "<Orden(nro_orden='%s', clase_orden='%s', descripcion='%s', ubic_tecnica='%s', ubic_tecnica_desc='%s', geo_x='%s', geo_y='%s', fecha_creacion='%s', fecha_ini_extremo='%s', fecha_fin_extremo='%s', calle='%s', altura='%s', clave_modelo_txt='%s', tipo_obra_id='%s', status_operacion='%s', comuna='%s')>" % (self.nro_orden, self.clase_orden, self.descripcion, self.ubic_tecnica, self.ubic_tecnica_desc, self.geo_x, self.geo_y, self.fecha_creacion,
+                                                                                                                                                                                                                                                                                                                               self.fecha_ini_extremo, self.fecha_fin_extremo, self.calle, self.altura, self.clave_modelo_txt, self.tipo_obra_id, self.status_operacion, self.comuna)
 
+
+class ClaveTipo(Base):
+    __tablename__ = 'clave_tipo'
+    clave_tipo_id = Column(
+        Integer, primary_key=True, autoincrement='ignore_fk')
+    clave = Column(String(50))
+    tipo_id = Column(Integer, ForeignKey('tipos_obra.tipo_id'))
+
+
+class StatusTraduccion(Base):
+    __tablename__ = 'status_traduccion'
+    status_traduccion_id = Column(
+        Integer, primary_key=True, autoincrement='ignore_fk')
+    status_usuario = Column(String(50))
+    status_id = Column(Integer, ForeignKey('status_obra.status_id'))
+
+# Crear todas las tablas necesarias para insertar los datos del WS
 Base.metadata.create_all(engine, checkfirst=True)
+
+# Agregar tipo de obra en caso de que este vacia
+ev = session.query(TipoObra.name).count()
+if not ev:
+    session.add_all([TipoObra(name='Bacheo'), TipoObra(name='Senalizacion'), TipoObra(
+        name='Tapas'), TipoObra(name='Materiales'), TipoObra(name='Vereda'), TipoObra(name='Cordon')])
+    session.commit()
+
+# Agregar status de obra en caso de que este vacia
+ev = session.query(StatusObra.name).count()
+if not ev:
+    session.add_all([StatusObra(name='Planificado'), StatusObra(
+        name='En ejecucion'), StatusObra(name='Finalizado'), StatusObra(name='Denegado')])
+    session.commit()
+
+# Chequear si ya se agrego el mapeo de clave modelo a tipo de obra
+ev = session.query(ClaveTipo.clave).count()
+if not ev:
+    # Si no existen, agregar a la tabla a partir del CSV
+    with open('clave-tipo.csv', 'rb') as csvfile:
+        mappings = csv.reader(csvfile, delimiter=',', quotechar='"')
+        for row in mappings:
+            session.add(ClaveTipo(**{"clave": row[0], "tipo_id": row[1]}))
+            session.commit()
+
+# Chequear si ya se agrego el mapeo de status_usuario a status de obra
+# legible por humanos
+ev = session.query(StatusTraduccion.status_usuario).count()
+if not ev:
+    # Si no existen, agregar a la tabla a partir del CSV
+    with open('status_usuario-status.csv', 'rb') as csvfile:
+        mappings = csv.reader(csvfile, delimiter=',', quotechar='"')
+        for row in mappings:
+            session.add(
+                StatusTraduccion(**{"status_usuario": row[0], "status_id": row[1]}))
+            session.commit()
+
+##################
+# END SQLALCHEMY #
+##################
 
 url = urlparse.urljoin(
     'file:', urllib.pathname2url(config.wsdl_path))
 client = Client(
     url, username=config.wsdl_username, password=config.wsdl_password)
 
-desde = "20150901"
-hasta = "20150902"
+desde = "20150629"
+hasta = "20150630"
 tipos_ordenes = ["ACME", "CAME"]
 modos = ["CREACION", "MODIFICACION"]
 columnas = ["NRO_ORDEN", "CLASE_ORDEN", "DESCRIPCION", "UBIC_TECNICA", "UBIC_TECNICA_DESC", "FECHA_CREACION", "FECHA_INI_EXTREMO",
@@ -80,6 +162,22 @@ for tipo_orden in tipos_ordenes:
                 # Si no existe el record en la DB, calcular geocodificacion,
                 # comuna, y agregar a la DB
 
+                # Mapear clave_modelo al tipo de obra definido
+                new_orden["tipo_obra_id"] = None
+                if new_orden["clave_modelo"] is not None:
+                    result = session.query(ClaveTipo).filter(
+                        ClaveTipo.clave == new_orden["clave_modelo"]).first()
+                    if result:
+                        new_orden["tipo_obra_id"] = result.tipo_id
+
+                # Mapear status_usuario al status humano definido
+                new_orden["status_id"] = None
+                if new_orden["status_usuario"] is not None:
+                    result = session.query(StatusTraduccion).filter(
+                        StatusTraduccion.status_usuario == new_orden["status_usuario"]).first()
+                    if result:
+                        new_orden["status_id"] = result.status_id
+
                 direccion_list = new_orden[
                     "ubic_tecnica_desc"].split("-")[0].split(" ")
                 altura = direccion_list[-1]
@@ -89,13 +187,14 @@ for tipo_orden in tipos_ordenes:
                 geocod = json.load(urllib2.urlopen(
                     'http://ws.usig.buenosaires.gob.ar/rest/normalizar_y_geocodificar_direcciones?calle=' + calle + '&altura=' + altura + '&desambiguar=1'))
                 if "Normalizacion" in geocod.keys():
-                    new_orden["geo_x"] = ""
-                    new_orden["geo_y"] = ""
                     new_orden["tipo_resultado"] = geocod[
                         "Normalizacion"]["TipoResultado"]
                 if "GeoCodificacion" in geocod.keys():
-                    new_orden["geo_x"] = geocod["GeoCodificacion"]["x"]
-                    new_orden["geo_y"] = geocod["GeoCodificacion"]["y"]
+                    if type(geocod["GeoCodificacion"]) is dict:
+                        new_orden["geo_x"] = geocod["GeoCodificacion"]["x"]
+                        new_orden["geo_y"] = geocod["GeoCodificacion"]["y"]
+                    else:
+                        new_orden["tipo_resultado"] = geocod["GeoCodificacion"]
 
                 comuna = "NA"
                 try:
