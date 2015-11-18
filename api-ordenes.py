b379689c22408507120f34623e3d7d5d555b4b34
Carolina Hadad <carolinahadad@gmail.com>
2015-11-18 19:12:14 -0300

    Agregado metodo para recalcular tipo de obra + refactor

--- api-ordenes.py
+++ api-ordenes.py
@@ -7,128 +7,17 @@ import os
 import json
 import urllib2
 import csv
+from data_models import *
 
-###########################################################
-# HUGE TODO: refactor sqlalchemy stuff to separate module #
-###########################################################
-
-from sqlalchemy import *
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
 
 engine = create_engine(config.db_url)
 # create a configured "Session" class
 Session = sessionmaker(bind=engine)
 # create a Session
 session = Session()
-Base = declarative_base()
 
-
-class TipoObra(Base):
-    __tablename__ = 'tipos_obra'
-    tipo_id = Column(Integer, primary_key=True, autoincrement='ignore_fk')
-    name = Column(String(50))
-
-    def __repr__(self):
-        return "<TipoObra(name='%s')>" % (self.name)
-
-
-class StatusObra(Base):
-    __tablename__ = 'status_obra'
-    status_id = Column(Integer, primary_key=True, autoincrement='ignore_fk')
-    name = Column(String(50))
-
-    def __repr__(self):
-        return "<StatusObra(name='%s')>" % (self.name)
-
-
-class Orden(Base):
-    __tablename__ = 'ordenes'
-    orden_id = Column(Integer, primary_key=True, autoincrement='ignore_fk')
-    nro_orden = Column(String(50))
-    clase_orden = Column(String(100))
-    descripcion = Column(String(100))
-    ubic_tecnica = Column(String(100))
-    ubic_tecnica_desc = Column(String(100))
-    geo_x = Column(String(20))
-    geo_y = Column(String(20))
-    tipo_resultado = Column(String(100))
-    fecha_creacion = Column(Date)
-    fecha_ini_extremo = Column(Date)
-    fecha_fin_extremo = Column(Date)
-    calle = Column(String(100))
-    altura = Column(String(10))
-    clave_modelo = Column(String(50))
-    clave_modelo_txt = Column(String(100))
-    tipo_obra_id = Column(Integer, ForeignKey('tipos_obra.tipo_id'))
-    area_empresa = Column(String(10))
-    status_usuario = Column(String(50))
-    status_id = Column(Integer, ForeignKey('status_traduccion.status_id'))
-    fecha_ult_modif = Column(Date)
-    comuna = Column(String(20))
-
-    def __repr__(self):
-        return "<Orden(nro_orden='%s', clase_orden='%s', descripcion='%s', ubic_tecnica='%s', ubic_tecnica_desc='%s', geo_x='%s', geo_y='%s', fecha_creacion='%s', fecha_ini_extremo='%s', fecha_fin_extremo='%s', calle='%s', altura='%s', clave_modelo_txt='%s', tipo_obra_id='%s', status_operacion='%s', comuna='%s')>" % (self.nro_orden, self.clase_orden, self.descripcion, self.ubic_tecnica, self.ubic_tecnica_desc, self.geo_x, self.geo_y, self.fecha_creacion,
-                                                                                                                                                                                                                                                                                                                               self.fecha_ini_extremo, self.fecha_fin_extremo, self.calle, self.altura, self.clave_modelo_txt, self.tipo_obra_id, self.status_operacion, self.comuna)
-
-
-class ClaveTipo(Base):
-    __tablename__ = 'clave_tipo'
-    clave_tipo_id = Column(
-        Integer, primary_key=True, autoincrement='ignore_fk')
-    clave = Column(String(50))
-    tipo_id = Column(Integer, ForeignKey('tipos_obra.tipo_id'))
-
-
-class StatusTraduccion(Base):
-    __tablename__ = 'status_traduccion'
-    status_traduccion_id = Column(
-        Integer, primary_key=True, autoincrement='ignore_fk')
-    status_usuario = Column(String(50))
-    status_id = Column(Integer, ForeignKey('status_obra.status_id'))
-
-# Crear todas las tablas necesarias para insertar los datos del WS
-Base.metadata.create_all(engine, checkfirst=True)
-
-# Agregar tipo de obra en caso de que este vacia
-ev = session.query(TipoObra.name).count()
-if not ev:
-    session.add_all([TipoObra(name='Bacheo'), TipoObra(name='Senalizacion'), TipoObra(
-        name='Tapas'), TipoObra(name='Materiales'), TipoObra(name='Vereda'), TipoObra(name='Cordon')])
-    session.commit()
-
-# Agregar status de obra en caso de que este vacia
-ev = session.query(StatusObra.name).count()
-if not ev:
-    session.add_all([StatusObra(name='Planificado'), StatusObra(
-        name='En ejecucion'), StatusObra(name='Finalizado'), StatusObra(name='Denegado')])
-    session.commit()
-
-# Chequear si ya se agrego el mapeo de clave modelo a tipo de obra
-ev = session.query(ClaveTipo.clave).count()
-if not ev:
-    # Si no existen, agregar a la tabla a partir del CSV
-    with open('clave-tipo.csv', 'rb') as csvfile:
-        mappings = csv.reader(csvfile, delimiter=',', quotechar='"')
-        for row in mappings:
-            session.add(ClaveTipo(**{"clave": row[0], "tipo_id": row[1]}))
-            session.commit()
-
-# Chequear si ya se agrego el mapeo de status_usuario a status de obra
-# legible por humanos
-ev = session.query(StatusTraduccion.status_usuario).count()
-if not ev:
-    # Si no existen, agregar a la tabla a partir del CSV
-    with open('status_usuario-status.csv', 'rb') as csvfile:
-        mappings = csv.reader(csvfile, delimiter=',', quotechar='"')
-        for row in mappings:
-            session.add(
-                StatusTraduccion(**{"status_usuario": row[0], "status_id": row[1]}))
-            session.commit()
-
-##################
-# END SQLALCHEMY #
-##################
+load_data()
 
 url = urlparse.urljoin(
     'file:', urllib.pathname2url(config.wsdl_path))
@@ -136,7 +25,7 @@ client = Client(
     url, username=config.wsdl_username, password=config.wsdl_password)
 
 desde = "20151103"
-hasta = "20151105"
+hasta = "20151104"
 tipos_ordenes = ["ACME", "CAME"]
 modos = ["CREACION", "MODIFICACION"]
 columnas = ["NRO_ORDEN", "CLASE_ORDEN", "DESCRIPCION", "UBIC_TECNICA", "UBIC_TECNICA_DESC", "FECHA_CREACION", "FECHA_INI_EXTREMO",
---
+++ recalcular_tipo_de_obra.py
@@ -0,0 +1,32 @@
+import csv
+import config
+
+from sqlalchemy.orm import sessionmaker
+
+from data_models import *
+
+engine = create_engine(config.db_url)
+# create a configured "Session" class
+Session = sessionmaker(bind=engine)
+# create a Session
+session = Session()
+Base = declarative_base()
+
+
+for row in session.query(Orden):
+    print "viejo"
+    print row.status_id
+
+    tipo = session.query(ClaveTipo).filter(
+        ClaveTipo.clave == row.clave_modelo).first()
+
+    if tipo:
+        tipo_id = tipo.tipo_id
+    else:
+        tipo_id = session.query(TipoObra).filter(
+            TipoObra.name == 'Otros').first().tipo_id
+
+    row.tipo_id = tipo_id
+
+    print "nuevo"
+    print tipo_id
